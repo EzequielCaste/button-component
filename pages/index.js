a8d794a439bd166d444132c974bfe39e1fddcbd6
EzequielCaste <ezequiel.caste@gmail.com>
2021-02-13 19:23:13 -0300

    create a reusable Button components
    Create a reusable button with all the states in the design and a page displaying all the states.

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,59 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { Button } from '../components/Button';
+import { Footer } from '../components/Footer';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="Container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
       </Head>
+      <h1>Buttons</h1>
+      <code>{`<Button />`}</code>
+      <Button />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <code>{`<Button variant="outline" />`}</code>
+      <Button variant="outline" />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <code>{`<Button variant="text" />`}</code>
+      <Button variant="text" />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <code>{`<Button disableShadow />`}</code>
+      <Button disableShadow />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <code>{`<Button disabled />`}</code>
+      <Button disabled />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+      <code>{`<Button variant="text" disabled />`}</code>
+      <Button variant="text" disabled />
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <code>{`<Button startIcon="local_grocery_store" />`}</code>
+      <Button startIcon="local_grocery_store" />
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <code>{`<Button endIcon="local_grocery_store" />`}</code>
+      <Button endIcon="local_grocery_store" />
+
+      <code>{`<Button size="sm" />`}</code>
+      <Button size="sm" />
+      <code>{`<Button size="md" />`}</code>
+      <Button size="md" />
+      <code>{`<Button size="lg" />`}</code>
+      <Button size="lg" />
+
+      <code>{`<Button color="default" />`}</code>
+      <Button color="default" />
+      <code>{`<Button color="primary" />`}</code>
+      <Button color="primary" />
+      <code>{`<Button color="secondary" />`}</code>
+      <Button color="secondary" />
+      <code>{`<Button color="danger" />`}</code>
+      <Button color="danger" />
+      <Footer />
     </div>
-  )
+  );
 }
